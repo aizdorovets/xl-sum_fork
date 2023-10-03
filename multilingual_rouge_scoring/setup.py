7b7cd8199d214c0f176042c620f0fccd6934f980
Alexander Zdorovets <loferist@gmail.com>
2023-10-03 18:17:51 -0400

    Update setup.py

--- multilingual_rouge_scoring/setup.py
+++ multilingual_rouge_scoring/setup.py
@@ -10,8 +10,8 @@ setuptools.setup(
     description="Pure python implementation of ROUGE-1.5.5.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=['rouge_score'],
-    package_dir = {'rouge_score':''},
+    packages=['multilingua_rouge_score'],
+    package_dir = {'multilingua_rouge_score':''},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License"
